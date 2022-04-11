a9a576127af2bd96cb35e3210b63765b34148ba1
Hamza_raspi <chebihamza926@gmail.com>
2022-04-11 16:25:56 +0100

    fixe the csv atttached file and chage it to html content

--- Main/mail_send/mail_send_ssl.py
+++ Main/mail_send/mail_send_ssl.py
@@ -14,7 +14,7 @@ import urllib.request
 
 def connect(host='http://google.com'):  # vérifier l'accés internet
     try:
-        urllib.request.urlopen(host)  # Python 3.x
+        urllib.request.urlopen(host)  # Python 3.xx
         return True
     except:
         return False
